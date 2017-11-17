517fbd33fb81c224014128559519098df18b2cc0
Peter Downs <peterldowns@gmail.com>
2017-11-17 15:40:04 -0500

    Add datastore admin views.

--- datastore/admin.py
+++ datastore/admin.py
@@ -2,5 +2,37 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Document
+from .models import Annotation
+from .models import Upload
+from .models import Archive
 
 # Register your models here.
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creator', 'state', 'created_at', 'updated_at')
+    list_filter = ('state', 'created_at', 'updated_at')
+    search_fields = ('id', 'creator', 'title',  'author')
+    ordering = ('id',)
+
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'document', 'creator', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('uuid', 'creator')
+    ordering = ('created_at',)
+
+class UploadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'creator', 'state', 'content_type', 'created_at', 'updated_at')
+    list_filter = ('state', 'created_at', 'updated_at')
+    search_fields = ('creator', 'state', 'content_type')
+    ordering = ('id',)
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'creator', 'title')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title',)
+    ordering = ('id',)
+
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Annotation, AnnotationAdmin)
+admin.site.register(Upload, UploadAdmin)
+admin.site.register(Archive, ArchiveAdmin)
--- datastore/models.py
+++ datastore/models.py
@@ -23,6 +23,8 @@ class Archive(models.Model):
     )
     title = models.TextField(null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
 
 
 class DocumentState(Enum):
