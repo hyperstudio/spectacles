922f0e7b8d17ee8697f135a897d8a4e0f5103e55
Peter <peterldowns@gmail.com>
2018-03-12 12:18:07 +0000

    Fix elasticsearch memory usage.

--- config/install-elasticsearch.sh
+++ config/install-elasticsearch.sh
@@ -1,2 +1,4 @@
 sudo cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.backup
 sudo cp ./elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /etc/elasticsearch/jvm.options /etc/elasticsearch/jvm.options.backup
+sudo cp ./es_jvm.options /etc/elasticsearch/jvm.options
